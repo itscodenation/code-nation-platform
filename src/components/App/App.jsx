534e6d640f77fe88556c8544461103ddbb4e347c
Mat Brown <mat.a.brown@gmail.com>
2019-02-20 13:42:32 -0500

    Don’t show sign in button until gapi is ready
    Also involves setting up our reducer for the first time

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,22 +2,51 @@ import React from 'react';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import noop from 'lodash-es/noop';
+import {useImmerReducer} from 'use-immer';
 
-import styles from './App.module.css';
 import {init as initGoogle} from '../../clients/google';
 import {signInWithGoogle} from '../../clients/firebase';
+import {useAsyncEffect} from 'use-async-effect';
 
-initGoogle();
+import styles from './App.module.css';
+
+function reducer(state, {type, payload = {}}) {
+  switch (type) {
+    case 'google-ready':
+      state.clients.google.ready = true;
+      break;
+    default:
+      break;
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useImmerReducer(
+    reducer,
+    {clients: {google: {ready: false}}},
+  );
+
+  useAsyncEffect(async () => {
+    await initGoogle();
+    dispatch({type: 'google-ready'});
+  }, noop, []);
+
   return (
     <div className={styles.container}>
-      <Form>
-        <p>Sign in with your Google account to get started:</p>
-        <Form.Group>
-          <Button onClick={signInWithGoogle}>Log in</Button>
-        </Form.Group>
-      </Form>
+      {
+        state.clients.google.ready ?
+          (
+            <Form>
+              <p>Sign in with your Google account to get started:</p>
+              <Form.Group>
+                <Button onClick={signInWithGoogle}>Log in</Button>
+              </Form.Group>
+            </Form>
+          ) : (
+            <div>Loading…</div>
+          )
+      }
     </div>
   );
 }
