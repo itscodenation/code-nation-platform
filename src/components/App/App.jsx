5ace0f88c75051ff390f4a651b152ea700939af4
Mat Brown <mat.a.brown@gmail.com>
2019-02-20 11:03:28 -0500

    Basic Google auth flow
    Lots of stuff cribbed from Popcode

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,15 +3,23 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+import {getGapiSync, loadAndConfigureGapi} from '../../services/gapi';
+
 import styles from './App.module.css';
 
-export default function App(props) {
+loadAndConfigureGapi();
+
+export default function App() {
   return (
     <div className={styles.container}>
       <Form>
         <p>Sign in with your Google account to get started:</p>
         <Form.Group>
-          <Button>Log in</Button>
+          <Button onClick={async() => {
+            const gapi = getGapiSync();
+            const auth = gapi.auth2.getAuthInstance();
+            await auth.signIn({prompt: 'select_account'});
+          }}>Log in</Button>
         </Form.Group>
       </Form>
     </div>
---
+++ src/services/gapi.js
@@ -0,0 +1,63 @@
+import loadjs from 'loadjs';
+import once from 'lodash-es/once';
+
+const GAPI_SCRIPT_URL = 'https://apis.google.com/js/api.js';
+const SCOPES = [
+  'https://www.googleapis.com/auth/classroom.courses.readonly',
+  'https://www.googleapis.com/auth/classroom.coursework.students',
+  'https://www.googleapis.com/auth/drive',
+];
+const DISCOVERY_DOCS = [
+  'https://www.googleapis.com/discovery/v1/apis/classroom/v1/rest',
+  'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
+];
+
+let isGapiLoadedAndConfigured = false;
+
+const loadGapi = once(async() => new Promise((resolve, reject) => {
+  loadjs(GAPI_SCRIPT_URL, {
+    success() {
+      resolve(window.gapi);
+    },
+    error(pathsNotFound) {
+      reject(`Could not load scripts ${pathsNotFound.join(', ')}`);
+    },
+  });
+}));
+
+export async function loadAndConfigureGapi() {
+  const gapi = await loadGapi();
+  await new Promise((resolve, reject) => {
+    gapi.load('client:auth2', {
+      callback() {
+        isGapiLoadedAndConfigured = true;
+        resolve(gapi);
+      },
+      onerror: reject,
+      timeout: 5000,
+      ontimeout() {
+        reject(new Error('Timed out'));
+      }
+    });
+  });
+
+  await gapi.client.init({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    clientId: process.env.REACT_APP_FIREBASE_CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scopes: SCOPES.join(' '),
+  });
+
+  await gapi.auth2.init({
+    client_id: process.env.REACT_APP_FIREBASE_CLIENT_ID,
+    scope: SCOPES.join(' '),
+    ux_mode: 'popup',
+  });
+}
+
+export function getGapiSync() {
+  if (!isGapiLoadedAndConfigured) {
+    throw new Error('gapi is not loaded');
+  }
+  return window.gapi;
+}
